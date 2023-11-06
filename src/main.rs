005a2b1cae6ccfcbd139c1664e6e7a25a35f2d69
Hugo Kapp <hugo.kapp6@hotmail.fr>
2023-11-06 20:59:45 +0100

    Read the words from the csv file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    for w in read_words() {
+        println!("{}", w);
+    }
+}
+
+type Word = String;
+
+fn read_words() -> Vec<Word> {
+    let input_file = "data/words.csv";
+    let mut csv_reader = csv::ReaderBuilder::new()
+                            .delimiter(b'\t')
+                            .from_path(input_file)
+                            .unwrap();
+
+    csv_reader.records()
+        .map(|res| res.unwrap())
+        .flat_map(|r| r.into_iter()
+                        .map(|s| String::from(s))
+                        .collect::<Vec<_>>()
+                        .into_iter())
+        .take(5)
+        .collect()
 }
