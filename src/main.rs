51f66d46f7f5b3d61c0378ef1f5b59be16f8e694
Hugo Kapp <hugo.kapp6@hotmail.fr>
2023-11-09 23:10:29 +0100

    Remove anagrams from the dataset and compute the permutations at the end
    Runtime: 30s (4x improvement (!))

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 use fixedbitset::FixedBitSet as BitSet;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 fn main() {
     let words = read_words();
-    let bs = build_bitsets(&words);
+    println!("Before anagram removal: {} words", words.len());
 
-    search(&words, &bs[..]);
+    let (red_words, anagrams) = remove_anagrams(words);
+    println!("After anagram removal: {} words", red_words.len());
+
+    let bs = build_bitsets(&red_words);
+
+    search(red_words, bs, anagrams);
 }
 
 type Word = String;
 
 fn read_words() -> Vec<Word> {
     fn all_letters_distinct(s: &str) -> bool {
+        // TODO this can be improved with word_as_bits()
         let chars_used: HashSet<_> = s.chars().collect();
         chars_used.len() == s.len()
     }
@@ -26,6 +32,46 @@ fn read_words() -> Vec<Word> {
                 .collect()
 }
 
+type Anagrams = HashMap<WordBits, Vec<Word>>;
+type WordBits = u32;  // 1 bit per letter
+
+fn remove_anagrams(words: Vec<Word>) -> (Vec<Word>, Anagrams) {
+    // 1. Convert the vector of words into a hashmap from bit format to word(s)
+    let mut anagrams = HashMap::new();
+    for w in words {
+        let word_bits = word_as_bits(&w);
+        match anagrams.get_mut(&word_bits) {
+            None => {
+                anagrams.insert(word_bits, vec![w]);
+            }
+            Some(v) => {
+                v.push(w);
+            }
+        }
+    }
+
+    // 2. Pick one word from each "bucket" to form the new word vector
+    let red_words = anagrams.values()
+                        .map(|v| v.first().unwrap().clone())
+                        .collect::<Vec<_>>();
+
+    return (red_words, anagrams);
+}
+
+fn word_as_bits(word: &Word) -> WordBits {
+    fn set_bit(bits: &mut WordBits, letter: char) {
+        let bit_pos = letter_index(letter);
+        let mask = 1 << bit_pos;
+        *bits |= mask;
+    }
+
+    let mut bits = 0;
+    word.chars()
+        .for_each(|letter| set_bit(&mut bits, letter));
+
+    return bits;
+}
+
 const NLETTERS: usize = 26;
 
 fn all_ones_bitset(num_bits: usize) -> BitSet {
@@ -34,7 +80,7 @@ fn all_ones_bitset(num_bits: usize) -> BitSet {
     return bs;
 }
 
-fn char_to_bitset_index(c: char) -> usize {
+fn letter_index(c: char) -> usize {
     c as usize - 'a' as usize
 }
 
@@ -49,19 +95,31 @@ fn build_bitsets(words: &[Word]) -> [BitSet; NLETTERS] {
         .enumerate()
         .flat_map(|(i, w)|
             w.chars()
-                .map(move |letter| (char_to_bitset_index(letter), i)))
+                .map(move |letter| (letter_index(letter), i)))
         .for_each(|(bs_idx, w_idx)| bitsets[bs_idx].set(w_idx, false));
 
     return bitsets;
 }
 
-fn search(all_words: &[Word], letter_filters: &[BitSet]) {
-    let all_candidates = all_ones_bitset(all_words.len());
-    let mut stack = Vec::new();
-    search_rec(&all_candidates, letter_filters, &mut stack, all_words);
+struct Resources {
+    words:          Vec<Word>,
+    letter_filters: [BitSet; NLETTERS],
+    anagrams:       Anagrams,
 }
 
-fn search_rec(curr_candidates: &BitSet, letter_filters: &[BitSet], curr_words: &mut Vec<usize>, all_words: &[Word]) {
+fn search(all_words: Vec<Word>, letter_filters: [BitSet; NLETTERS], anagrams: Anagrams) {
+    let rsc = Resources {
+        words: all_words,
+        letter_filters,
+        anagrams
+    };
+
+    let all_candidates = all_ones_bitset(rsc.words.len());
+    let mut stack = Vec::new();
+    search_rec(&all_candidates, &mut stack, &rsc);
+}
+
+fn search_rec(curr_candidates: &BitSet, curr_words: &mut Vec<usize>, rsc: &Resources) {
     #[allow(unused_parens)]
     let final_step = (curr_words.len() == 4);
 
@@ -72,33 +130,60 @@ fn search_rec(curr_candidates: &BitSet, letter_filters: &[BitSet], curr_words: &
         if final_step {
             // TODO assert that the solutions make sense
             curr_words.push(i);
-            println!("Solution: {:?}", curr_words.iter().map(|widx| &all_words[*widx]).collect::<Vec<_>>());
+            solution(&curr_words, rsc);
             curr_words.pop();
         }
         else {
             // Recurse
             // Optimization: perform the filtering of previous words as we go
             rec_candidates.set_range(last_i..i, false);
-            let new_candidates = filter_candidates(&rec_candidates, i, all_words, letter_filters);
+            let new_candidates = filter_candidates(&rec_candidates, i, rsc);
             curr_words.push(i);
-            search_rec(&new_candidates, letter_filters, curr_words, all_words);
+            search_rec(&new_candidates, curr_words, rsc);
             curr_words.pop();
         }
         last_i = i;
     }
 }
 
-fn filter_candidates(curr_candidates: &BitSet, new_word_idx: usize, all_words: &[Word], letter_filters: &[BitSet]) -> BitSet {
+fn filter_candidates(curr_candidates: &BitSet, new_word_idx: usize, rsc: &Resources) -> BitSet {
     let mut new_candidates = curr_candidates.clone();
 
     use std::ops::BitAndAssign;
-    let new_word = &all_words[new_word_idx];
+    let new_word = &rsc.words[new_word_idx];
     new_word.chars()
         .map(|letter| {
-            let bs_idx = char_to_bitset_index(letter);
-            &letter_filters[bs_idx]
+            let bs_idx = letter_index(letter);
+            &rsc.letter_filters[bs_idx]
         })
         .for_each(|filter| new_candidates.bitand_assign(filter));
 
     return new_candidates;
+}
+
+fn solution(solution: &[usize], rsc: &Resources) {
+    // Build all the permutations using the anagrams
+    // This is all very reminiscent of the initial algorithm...
+    fn permutations<'a>(resolved_words: &mut Vec<&'a Word>, init_solution: &[usize], rsc: &'a Resources) {
+        let curr_level = resolved_words.len();
+        #[allow(unused_parens)]
+        let final_step = (curr_level == (init_solution.len() - 1));
+        let word_index = init_solution[curr_level];
+        let word = &rsc.words[word_index];
+        let word_bits = word_as_bits(word);
+        let word_anagrams = rsc.anagrams.get(&word_bits).unwrap();
+        for word_collision in word_anagrams {
+            resolved_words.push(word_collision);
+            if final_step {
+                println!("Solution: {:?}", resolved_words);
+            }
+            else {
+                permutations(resolved_words, init_solution, rsc);
+            }
+            resolved_words.pop();
+        }
+    }
+
+    let mut v = Vec::new();
+    permutations(&mut v, solution, rsc);
 }
