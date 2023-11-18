c104e4c404f09ca82129cb60b0a7727fb1cfa166
Hugo Kapp <hugo.kapp6@hotmail.fr>
2023-11-18 09:36:09 +0100

    Implement anagram permutations
    Solution now scales to 50k words (out of 350k)

--- hs/Main.hs
+++ hs/Main.hs
@@ -3,11 +3,16 @@ import Control.Monad(join)
 import System.IO.Unsafe(unsafePerformIO)
 import Data.Char(isLetter)
 import Data.Functor((<&>))
+import Data.Map (Map, (!))
+import Data.Set (Set)
+import qualified Data.Map as Map
 
 main =
   do
     allWords <- readAll
-    let solutions = search $ preprocess allWords
+    let (consideredWords, anagrams) = preprocess allWords
+    let solutionSubset = search consideredWords
+    let solutions = solutionSubset >>= permutations anagrams
     print solutions
     print $ length solutions
 
@@ -19,15 +24,28 @@ readAll =
     let fileLines = lines fileContent
     -- Remove '\r' characters in every line
     let actualWords = fileLines <&> (filter isLetter)
-    return $ take 40000 actualWords
+    return $ take 60000 actualWords
 
-preprocess :: [String] -> [String]
+type Anagrams = Map (Set Char) [String]
+
+preprocess :: [String] -> ([String], Anagrams)
 preprocess =
   let
     fiveLetters w = length w == 5
     noDup w = length (Set.fromList w) == 5
   in
-    filter noDup . filter fiveLetters
+    reduceAnagrams . filter noDup . filter fiveLetters
+
+reduceAnagrams :: [String] -> ([String], Anagrams)
+reduceAnagrams allWords =
+  let
+    anagrams = groupIntoMap Set.fromList allWords
+    reducedWords = head <$> Map.elems anagrams
+  in
+    (reducedWords, anagrams)
+
+groupIntoMap :: (Ord k) => (a -> k) -> [a] -> Map k [a]
+groupIntoMap f = Map.fromListWith (++) . map (\x -> (f x, [x]))
 
 search :: [String] -> [[String]]
 search ws = searchRec ws []
@@ -45,6 +63,10 @@ searchRec allWords currSol =
     candidates >>= (\w -> searchRec allWords (w:currSol))
 
 flatten = join
+
+permutations :: Anagrams -> [String] -> [[String]]
+permutations anagrams [] = []
+permutations anagrams (w:ws) = (anagrams ! (Set.fromList w)) >>= (\q -> map ((:) q) (permutations anagrams ws))
 
 debug x y =
   unsafePerformIO $
